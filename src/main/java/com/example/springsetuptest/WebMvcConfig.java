d302e37ecbb1c00e872e343616d17b64a8134ca2
kamarbaraka <kamar254baraka@gmail.com>
2023-08-08 16:49:56 +0300

    added views
    added a test view
    
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

---
+++ src/main/java/com/example/springsetuptest/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.springsetuptest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.springsetuptest")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
